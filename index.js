1c850441de26e3ef1d6424b3f734df66d5f55992
Manav Sarkar <42129636+ManavSarkar@users.noreply.github.com>
2024-01-09 11:35:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const PORT = process.env.PORT || 5000;
 app.use(
     cors({
         origin: [
-            "http://localhost:3000",
             "https://digilabs-landing-frontend.vercel.app",
 
         ],
